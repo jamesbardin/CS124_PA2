d1a51e6e408041e2de3d581a1bbddb9d648c27fd
jamesbardin <jbardin@college.harvard.edu>
2023-03-28 00:00:38 -0400

    added padding

--- strassen_jim.py
+++ strassen_jim.py
@@ -44,10 +44,55 @@ def strassen_multiply(A, B, n0):
         C[mid:, mid:] = P1 - P2 + P3 + P6
         
         return C
-    
 
-A = np.random.randint(0, 2, size=(128, 128))
-B = np.random.randint(0, 2, size=(128, 128))
+def strassen_multiply_pad(A, B, n0):
+    n = max(A.shape[0], A.shape[1], B.shape[1])
+    m = 1
+    while m < n:
+        m *= 2
+
+    A_pad = np.zeros((m, m))
+    A_pad[:A.shape[0], :A.shape[1]] = A
+
+    B_pad = np.zeros((m, m))
+    B_pad[:B.shape[0], :B.shape[1]] = B
+
+    if m <= n0:
+        return np.dot(A_pad, B_pad)
+
+    else:
+        mid = m // 2
+
+        A11 = A_pad[:mid, :mid]
+        A12 = A_pad[:mid, mid:]
+        A21 = A_pad[mid:, :mid]
+        A22 = A_pad[mid:, mid:]
+
+        B11 = B_pad[:mid, :mid]
+        B12 = B_pad[:mid, mid:]
+        B21 = B_pad[mid:, :mid]
+        B22 = B_pad[mid:, mid:]
+
+        P1 = strassen_multiply(A11 + A22, B11 + B22, n0)
+        P2 = strassen_multiply(A21 + A22, B11, n0)
+        P3 = strassen_multiply(A11, B12 - B22, n0)
+        P4 = strassen_multiply(A22, B21 - B11, n0)
+        P5 = strassen_multiply(A11 + A12, B22, n0)
+        P6 = strassen_multiply(A21 - A11, B11 + B12, n0)
+        P7 = strassen_multiply(A12 - A22, B21 + B22, n0)
+
+        C = np.zeros((m, m))
+
+        C[:mid, :mid] = P1 + P4 - P5 + P7
+        C[:mid, mid:] = P3 + P5
+        C[mid:, :mid] = P2 + P4
+        C[mid:, mid:] = P1 - P2 + P3 + P6
+
+        return C[:A.shape[0], :B.shape[1]]
+
+n = 256
+A = np.random.randint(0, 2, size=(n, n))
+B = np.random.randint(0, 2, size=(n, n))
 # A = np.array([[1, 2], [3, 4]])
 # B = np.array([[4, 3], [2, 1]])
 n0 = 23
@@ -57,7 +102,7 @@ n0 = 23
 r_std = standard_multiply(A, B)
 print(r_std)
 
-r_stsn = strassen_multiply(A, B, n0)
+r_stsn = strassen_multiply_pad(A, B, n0)
 print(r_stsn)
 
 print(np.array_equal(r_std, r_stsn))
